3f23226d3a3fa5501bc21cdf1f35dd0ff06b15d9
7srik <114638690+7srik@users.noreply.github.com>
2022-11-23 01:43:20 -0500

    Adding Indexes, 2nd Commit

---
+++ ProjectIndex.sql
@@ -0,0 +1,11 @@
+create nonclustered index HotelName
+on Hotel (HotelName ASC); 
+
+create nonclustered index DOB
+on Customer (DOB ASC); 
+
+create nonclustered index Persons
+on Room (NumberOfPersons ASC); 
+
+create nonclustered index RAvailable
+on Room (Availablity ASC);
